5d663cca51803b5cb42fb5a45f4f6c06798f79bf
Wahyu Sigit <wahyusigitpriyadi@gmail.com>
2021-06-24 15:00:11 +0700

    Create access-panel-without-port-8090.sh

---
+++ access-panel-without-port-8090.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo Panel Domain/Subdomain (without http/https)?
+read var1
+echo -e "extprocessor cyberpanel { \n" >> /usr/local/lsws/conf/httpd_config.conf
+echo -e "  type                    proxy\n" >> /usr/local/lsws/conf/httpd_config.conf
+echo -e "  address                 https://$var1:8090\n" >> /usr/local/lsws/conf/httpd_config.conf
+echo -e "  maxConns                100\n" >> /usr/local/lsws/conf/httpd_config.conf
+echo -e "  pcKeepAliveTimeout      60\n" >> /usr/local/lsws/conf/httpd_config.conf
+echo -e "  initTimeout             60\n" >> /usr/local/lsws/conf/httpd_config.conf
+echo -e "  retryTimeout            0\n" >> /usr/local/lsws/conf/httpd_config.conf
+echo -e "  respBuffer              0\n" >> /usr/local/lsws/conf/httpd_config.conf
+echo -e "}\n" >> /usr/local/lsws/conf/httpd_config.conf
+
+echo -e "REWRITERULE ^(.*)$ HTTP://cyberpanel/$1 [P]\n" >> /home/$var1/public_html/.htaccess
+
